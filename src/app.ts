4e9470469fbd8e6ccb82f59d5bc6ef0ddc891d8a
Tom Lloyd <thomas.lloyd@sky.uk>
2018-10-28 18:02:32 +0000

    Switched from Javascript to Typescript, including changing Eslint to Tslint

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Listening on port ${port}...`)); // tslint:disable-line:no-console
