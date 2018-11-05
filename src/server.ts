38bdb7604832e09e9c1370959f126ab89d8239b2
Tom Lloyd <thomas.lloyd@sky.uk>
2018-11-05 00:11:16 +0000

    Almost finished circleci pipeline to push image to ECR and deploy to ECS

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello, World!"));
+app.get("/", (req, res) => res.send("Hello from the command line!"));
 
 app.listen(port, () => console.log(`Listening on port ${port}...`)); // tslint:disable-line:no-console
