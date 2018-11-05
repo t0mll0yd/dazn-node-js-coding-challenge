50bc08d481e1dc04ccb3b4ab92667fbeeee98994
Tom Lloyd <tom.w.lloyd@googlemail.com>
2018-11-05 01:40:29 +0000

    Completed README.md with API definition and added correct path for API

--- component-tests/server.spec.ts
+++ component-tests/server.spec.ts
@@ -3,12 +3,15 @@ import * as supertest from "supertest";
 const request = supertest(process.env.API_LOCATION || 'http://localhost:3000');
 
 describe("Server", () => {
-    test("path '/' returns 200 Hello World", () => {
+    test("path '/' returns 404 NOT FOUND", () => {
         return request
             .get('/')
-            .expect(200)
-            .then(response => {
-                expect(response.text).toBe("Hello from CIRCLE CI");
-            });
+            .expect(404)
+    });
+
+    test("path '/users/USER_ID/streams/STREAM_ID' returns 201 CREATED", () => {
+        return request
+            .get('/users/1/streams/1')
+            .expect(201)
     });
 });
