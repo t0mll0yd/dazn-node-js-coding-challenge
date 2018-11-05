d49866fc13b5807243c563fd5720d57b5b4cead9
Tom Lloyd <thomas.lloyd@sky.uk>
2018-11-05 01:38:38 +0000

    Added correct path

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello from CIRCLE CI"));
+app.get("/users/:userId/streams/:streamId", (req, res) => {
+    res.statusCode = 201;
+    res.send();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`)); // tslint:disable-line:no-console
