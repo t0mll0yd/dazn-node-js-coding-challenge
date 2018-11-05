bfd9b100edf1683ac4ee231d10bee5db0410f0b6
Tom Lloyd <thomas.lloyd@sky.uk>
2018-11-05 03:05:56 +0000

    fixed tests

--- src/stores/InMemoryStore.ts
+++ src/stores/InMemoryStore.ts
@@ -4,7 +4,7 @@ export class InMemoryStore implements IStore {
     private store: { [userId: string]: Set<string> } = {};
 
     public getStreams(userId: string): Set<string> {
-        return this.store[userId];
+        return this.store[userId] || new Set();
     }
 
     public addStream(userId: string, streamId: string): void {
